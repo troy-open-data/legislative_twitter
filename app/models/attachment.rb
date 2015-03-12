5f8171d3672b31003092afe56e40867bf46b2997
Lee <lee@leesharma.com>
2015-03-12 16:05:32 -0400

    add size validation for file uploads

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -2,6 +2,8 @@ class Attachment < ActiveRecord::Base
   belongs_to :legislation
   has_attached_file :file
 
+  validates_with AttachmentSizeValidator, :attributes => :file,
+                 :less_than => 1.megabytes
   validates_attachment_content_type :file, :content_type => [ "image/jpg",
                                                               "image/jpeg",
                                                               "image/png",
