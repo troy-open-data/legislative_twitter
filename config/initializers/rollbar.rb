8ee054136a244d8e0b2fe56aac9b02683d2d897a
Lee <lee@leesharma.com>
2015-03-13 11:52:27 -0400

    add env variable for rollbar

--- config/initializers/rollbar.rb
+++ config/initializers/rollbar.rb
@@ -2,8 +2,8 @@ require 'rollbar/rails'
 Rollbar.configure do |config|
   # Without configuration, Rollbar is enabled in all environments.
   # To disable in specific environments, set config.enabled=false.
-  
-  config.access_token = '3611d36581e54ba5a5a480e5e68b3f24'
+
+  config.access_token = ENV['ROLLBAR_ACCESS_TOKEN']
 
   # Here we'll disable in 'test':
   if Rails.env.test?
