56b9d394aec4739a820257302ebd03354a4be05f
Lee <lee@leesharma.com>
2015-03-15 14:31:59 -0400

    re-add prawnify_paragraphs method that was accidentily deleted

--- app/helpers/legislations_helper.rb
+++ app/helpers/legislations_helper.rb
@@ -49,6 +49,24 @@ module LegislationsHelper
       "#{attachment.title} (#{attachment.file.content_type})"
     end
   end
+  # Takes input in the form of an html-rich string and returns an array of
+  # prawn-sanitized paragraphs
+  def prawnify_paragraphs(body, size=16)
+    # Replace all headers with large text
+    body = body.gsub(/\<h\d\>/,"<font size='#{size}'>").gsub(/\<\/h\d\>/,'</font><p>')
+
+    # Remove all unexpected tags
+    body = body.split(/\<\/?p\>/).delete_if{|p| p.empty?}
+
+    body.map! do |paragraph|
+      Sanitize.fragment(paragraph,
+                        elements: %w{b i u strikethrough sub sup color font},
+                        attributes: {
+                            'font' => %w{size},
+                            'color' => %w{}
+                        })
+    end
+  end
 
 
 
