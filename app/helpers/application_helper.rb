26f33c3a0ad67c137e8ee65fc24b54745eeeeb7d
Lee <lee@leesharma.com>
2015-03-15 12:31:12 -0400

    add build count and status to footer

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,20 @@
 module ApplicationHelper
   # Returns a string of version information for the footer
+  ENV['HEROKU_APP_NAME']='legislative-twitter-dev'
+  ENV['HEROKU_API_KEY']='88255870-6eeb-4a96-a014-be45352cb5e0'
   def heroku_version
     begin
+      # Read in values from environment variables
       released_at = DateTime.rfc3339(ENV['HEROKU_RELEASED_AT']).
         in_time_zone('Eastern Time (US & Canada)').
         strftime("%-l:%M%P %b %-d, %Y")
-      "Last updated #{released_at}"
+      build_count = ENV['HEROKU_BUILD_COUNT'] || '?'
+      build_status = ENV['HEROKU_BUILD_STATUS'] || 'unknown'
+
+      # String construction
+      "Last updated #{released_at} (build ##{build_count} #{build_status})"
     rescue StandardError
-      'Last updated time unavailable'
+      'Previous build information unavailable'
     end
   end
 end
--- config/initializers/heroku.rb
+++ config/initializers/heroku.rb
@@ -5,7 +5,15 @@ unless (app_name = ENV['HEROKU_APP_NAME']).nil?
   require 'platform-api'
 
   heroku = PlatformAPI.connect(ENV['HEROKU_API_KEY'])
-  release = heroku.app.info(app_name)
 
-  ENV['HEROKU_RELEASED_AT'] = release['released_at']
+  # Fetch lsit of builds
+  build_list = heroku.build.list(app_name)
+
+  # Determine buld count and most recent build
+  build_count = build_list.count
+  last_build = build_list.last
+
+  ENV['HEROKU_BUILD_COUNT'] = build_count.to_s
+  ENV['HEROKU_BUILD_STATUS'] = last_build['status'].to_s
+  ENV['HEROKU_RELEASED_AT'] = last_build['updated_at'].to_s
 end
