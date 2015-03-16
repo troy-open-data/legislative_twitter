188a451de9d52d910f2fd3d14da11b948a3a458b
Lee <lee@leesharma.com>
2015-03-15 21:25:34 -0400

    ui view improvements - ui: added foundation formatting to some forms - ui: various ui tweaks - fix: change level field to select box - add: admin submenu

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -11,7 +11,7 @@
 
 class Organization < ActiveRecord::Base
   # Model Variables
-  LEVEL_NAMES=['Mayor', 'General Assembly', 'Committee', 'Subcommittee']
+  LEVELS=['Mayor', 'General Assembly', 'Committee', 'Subcommittee']
 
   # Model Relationships
   has_many :meetings
