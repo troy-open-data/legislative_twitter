984c8a92f866b29f582c6157e547ab997444f98b
Lee <lee@leesharma.com>
2015-05-01 13:30:03 -0400

    add descriptions to organizations - add: migration - add: description to view form and show - add: whitelist description in controller

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -72,6 +72,6 @@ class OrganizationsController < ApplicationController
   # Never trust parameters from the scary internet,
   #   only allow the white list through.
   def organization_params
-    params.require(:organization).permit(:level, :name)
+    params.require(:organization).permit(:level, :name, :description)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150430195521) do
+ActiveRecord::Schema.define(version: 20150501172430) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -128,8 +128,9 @@ ActiveRecord::Schema.define(version: 20150430195521) do
   create_table "organizations", force: :cascade do |t|
     t.integer  "level"
     t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.text     "description"
   end
 
   create_table "people", force: :cascade do |t|
