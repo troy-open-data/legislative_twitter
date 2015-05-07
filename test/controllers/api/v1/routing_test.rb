f5a68c7ec643e6304d274fb982e669b3e759268f
Lee Sharma <lee@leesharma.com>
2015-05-07 13:41:03 -0400

    fix api v1 routes tests
    -add: subdomain option to #to action
    -add: full url (with subdomain) to each get request
    -add: explicitly set @controller for each api

--- test/controllers/api/v1/routing_test.rb
+++ test/controllers/api/v1/routing_test.rb
@@ -1,44 +1,56 @@
-# TODO: Figure out how to test routes with subdomains
-# require 'test_helper'
-#
-# module API
-#   module V1
-#     class RoutingTest < ActionController::TestCase
-#       context 'API' do
-#         setup { @request.host = 'http://api.example.com' }
-#         context 'defaults' do
-#           should 'return json' do
-#             get api_v1_root_path
-#             assert_equal Mime::JSON, response.content_type
-#           end
-#           should 'be v1'
-#         end
-#         context 'v1' do
-#           context 'Data endpoint' do
-#             should route(:get, '/v1').to('data#index', format: :json)
-#           end
-#           context 'Bills API endpoints' do
-#             should route(:get, '/api/v1/bills').to(action: :index, format: :json)
-#             should route(:get, '/api/v1/bills/1')
-#                        .to(action: :show, id: 1, format: :json)
-#           end
-#           context 'Meetings endpoints' do
-#             should route(:get, '/v1/meetings').to('meeting#index', format: :json)
-#             should route(:get, '/v1/meetings/1')
-#                        .to('meeting#show', id: 1, format: :json)
-#           end
-#           context 'Organizations API endpoints' do
-#             should route(:get, '/api/v1/organizations').to(action: :index, format: :json)
-#             should route(:get, '/api/v1/organizations/1')
-#                        .to(action: :show, id: 1, format: :json)
-#           end
-#           context 'People API endpoints' do
-#             should route(:get, '/api/v1/people').to(action: :index, format: :json)
-#             should route(:get, '/api/v1/people/1')
-#                        .to(action: :show, id: 1, format: :json)
-#           end
-#         end
-#       end
-#     end
-#   end
-# end
+require 'test_helper'
+
+module API
+  module V1
+    class RoutingTest < ActionController::TestCase
+      context 'API' do
+        context 'defaults' do
+          context 'v1' do
+            # TODO: should respond with json
+          end
+          # TODO: should default to v1
+        end
+
+        context 'v1' do
+          context 'Data API endpoint' do
+            setup { @controller = API::V1::DataController }
+            should route(:get, 'http://api.example.com/v1')
+                     .to(action: :index, subdomain: 'api', format: :json)
+          end
+
+          context 'Bills API endpoints' do
+            setup { @controller = API::V1::BillsController }
+            should route(:get, 'http://api.example.com/v1/bills')
+                     .to(action: :index, subdomain: 'api', format: :json)
+            should route(:get, 'http://api.example.com/v1/bills/1')
+                     .to(action: :show, id: 1, subdomain: 'api', format: :json)
+          end
+
+          context 'Meetings API endpoints' do
+            setup { @controller = API::V1::MeetingsController }
+            should route(:get, 'http://api.example.com/v1/meetings')
+                     .to(action: :index, subdomain: 'api', format: :json)
+            should route(:get, 'http://api.example.com/v1/meetings/1')
+                     .to(action: :show, id: 1, subdomain: 'api', format: :json)
+          end
+
+          context 'Organizations API endpoints' do
+            setup { @controller = API::V1::OrganizationsController }
+            should route(:get, 'http://api.example.com/v1/organizations')
+                     .to(action: :index, subdomain: 'api', format: :json)
+            should route(:get, 'http://api.example.com/v1/organizations/1')
+                     .to(action: :show, id: 1, subdomain: 'api', format: :json)
+          end
+
+          context 'People API endpoints' do
+            setup { @controller = API::V1::PeopleController }
+            should route(:get, 'http://api.example.com/v1/people')
+                     .to(action: :index, subdomain: 'api', format: :json)
+            should route(:get, 'http://api.example.com/v1/people/1')
+                     .to(action: :show, id: 1, subdomain: 'api', format: :json)
+          end
+        end
+      end
+    end
+  end
+end
