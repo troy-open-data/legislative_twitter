3d5a6a31b9d491ad1c88fee1b37f402c06dc59cb
Lee Sharma <lee@leesharma.com>
2015-05-07 11:48:34 -0400

    add allow_requests_from_origin matcher
    - add: matcher content
    - fix: move matcher to shoulda module
    - remove: controller_matchers manifest file

--- test/matchers/controller/allow_requests_from_origin_matcher.rb
+++ test/matchers/controller/allow_requests_from_origin_matcher.rb
@@ -1,7 +1,75 @@
-module Matchers
-  module Controller
-    module AllowRequestsFromOrigin
-      puts 'Allow Requests From Origin is included'
+module Shoulda
+  module Matchers
+    module ActionController
+      # The `allow_requests_from_origin` matcher tests the CORS configuration
+      # of the resource associated with a request.
+      #
+      # You can specify the origin as a string:
+      #
+      #     # RSpec
+      #     describe PostsController do
+      #       describe 'GET #index' do
+      #         before { get :index }
+      #
+      #         it { should allow_requests_from('*') }
+      #       end
+      #     end
+      #
+      #     # Test::Unit
+      #     class PostsControllerTest < ActionController::TestCase
+      #       context 'GET #index' do
+      #         setup { get :index }
+      #
+      #         should allow_requests_from('*')
+      #       end
+      #     end
+      #
+      # @return [AllowRequestsFromOriginMatcher]
+      #
+      def allow_requests_from_origin(status)
+        AllowRequestsFromOriginMatcher.new(status)
+      end
+
+      # @private
+      class AllowRequestsFromOriginMatcher
+        def initialize(origin)
+          @origin = origin
+        end
+
+        def matches?(controller)
+          @controller = controller
+          correct_origin?
+        end
+
+        def failure_message
+          "Expected #{expectation}"
+        end
+        alias failure_message_for_should failure_message
+
+        def failure_message_when_negated
+          "Did not expect #{expectation}"
+        end
+        alias failure_message_for_should_not failure_message_when_negated
+
+        def description
+          "allow requests from origin \"#{@origin}\""
+        end
+
+        protected
+
+        def correct_origin?
+          access_control_allow_origin == @origin
+        end
+
+        def access_control_allow_origin
+          @controller.response.headers['Access-Control-Allow-Origin']
+        end
+        alias_method :allowed_origin, :access_control_allow_origin
+
+        def expectation
+          "allowed origin to be \"#{@origin}\", but was \"#{allowed_origin}\""
+        end
+      end
     end
   end
 end
